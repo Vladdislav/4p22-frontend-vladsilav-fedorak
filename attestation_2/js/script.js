9d3c000e6c96ac802740326c1901cdd032d6be05
Vladislav <morisonadd@mail.ru>
2022-11-17 20:52:51 +0300

    fixed counter simbol password

--- attestation_2/js/script.js
+++ attestation_2/js/script.js
@@ -12,7 +12,6 @@ let passwordConfirmError = document.querySelector(
   ".registration__password-confirm-error"
 );
 
-
 function validateEmail(email) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -30,10 +29,10 @@ function addBaseStyle(placeholder, displayForSpan) {
 formReg.addEventListener("submit", (event) => {
   event.preventDefault();
   genderInput.forEach((item) => {
-    if (item.checked){
-        genderInput = item.value;
+    if (item.checked) {
+      genderInput = item.value;
     }
-})
+  });
 
   emailInput.value?.trim();
   passwordError.value?.trim();
@@ -56,22 +55,27 @@ formReg.addEventListener("submit", (event) => {
     passwordError.innerHTML = "Поле обязательно для заполнения";
     addRedStylesError(passwordInput, passwordError);
   }
-  if(passwordInput.value.length < 8){
+  if (passwordInput.value.length <= 7) {
     passwordError.style.display = "inline";
     passwordError.innerHTML = "Пароль должен содержать не менее 8 символов";
     addRedStylesError(passwordInput, passwordError);
+  }
+  else {
+    addBaseStyle(passwordInput, passwordError);
   }
   if (confirmPasswordInput.value === "") {
     passwordConfirmError.style.display = "inline";
     passwordConfirmError.innerHTML = "Поле обязательно для заполнения";
     addRedStylesError(confirmPasswordInput, passwordConfirmError);
-  } else if (passwordInput.value === confirmPasswordInput.value && passwordInput.value.length >= 8) {
-    addBaseStyle(passwordInput,passwordError);
-    addBaseStyle(confirmPasswordInput,passwordConfirmError);
+  } else if (
+    passwordInput.value === confirmPasswordInput.value &&
+    passwordInput.value.length >= 7
+  ) {
+    addBaseStyle(passwordInput, passwordError);
+    addBaseStyle(confirmPasswordInput, passwordConfirmError);
     passwordError.style.color = "#787878";
     passwordConfirmError.style.color = "#787878";
-  }
-    else {
+  } else {
     passwordConfirmError.style.display = "inline";
     passwordConfirmError.innerHTML = "Пароли не совпадают";
     addRedStylesError(confirmPasswordInput, passwordConfirmError);
